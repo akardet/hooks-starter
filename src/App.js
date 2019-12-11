1e5ac00cec902d70263dbe5481565cb0666f133c
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-12-10 23:24:51 -0600

    add: toggle to homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
+import Toggle from "./Toggle";
 
 const App = () => {
+  //const [value, setValue] = useState(initialState);
+  const [name, setName] = useState("");
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
+      <h3>{name}</h3>
+      <input type="text" onChange={e => setName(e.target.value)} value={name} />
     </div>
   );
 };
