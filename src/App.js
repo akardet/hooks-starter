05d5497d5fd3e2f92ec0db8a8ed63fae3119af52
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-12-26 14:54:15 -0600

    add user context to toggle component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, createContext } from "react";
 import Toggle from "./Toggle";
 import { useTitleInput } from "./hooks/useTitleInput";
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
 
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => console.log(ref.current.className)}>
-        Level Up Dishes
-      </h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => console.log(ref.current.className)}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
